08ba8562a24b265e6cb996037df3a32205e37afa
ISOMARCO <ismayilnagiyev100@gmail.com>
2024-09-17 21:05:57 +0400

    1

---
+++ icare/DTOs/RentTableDto.cs
@@ -0,0 +1,15 @@
+﻿namespace icare.DTOs
+{
+    public class RentTableDto
+    {
+        public string Number { get; set; }
+        public string Date { get; set; }
+        public string InvoiceNumber { get; set; }
+        public string Customer { get; set; }
+        public string Address { get; set; }
+        public string Executor { get; set; }
+        public string Amount { get; set; }
+        public string Payment { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ icare/Pages/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using icare.DTOs;
+using System.Collections.ObjectModel;
+
+namespace icare;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<RentTableDto> DataList { get; set; }
+
+    public MainPage()
+    {
+        InitializeComponent();
+        DataList = new ObservableCollection<RentTableDto>
+            {
+                new() { Number = "1", Date = "01/09/2024", InvoiceNumber = "QA123", Customer = "Müştəri 1", Address = "Bakı", Executor = "İcraçı 1", Amount = "100₼", Payment = "Ödənildi", Note = "Qeyd yoxdur" },
+                new() { Number = "2", Date = "02/09/2024", InvoiceNumber = "QA124", Customer = "Müştəri 2", Address = "Sumqayıt", Executor = "İcraçı 2", Amount = "200₼", Payment = "Gözləyir", Note = "Ödəniş gözləyir" }
+            };
+        BindingContext = this;
+    }
+
+}
